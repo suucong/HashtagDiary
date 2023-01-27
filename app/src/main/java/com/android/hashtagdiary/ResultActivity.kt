fe3679b5e79cd87be8aaeb8e338151e28d2c1e8a
suucong <sophia021210@naver.com>
2023-01-27 14:07:33 +0900

    지도 API 추가

--- app/src/main/java/com/android/hashtagdiary/ResultActivity.kt
+++ app/src/main/java/com/android/hashtagdiary/ResultActivity.kt
@@ -2,10 +2,27 @@ package com.android.hashtagdiary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import net.daum.mf.map.api.MapView
+import net.daum.mf.map.api.MapPoint
 
 class ResultActivity : AppCompatActivity() {
+    lateinit var tvDate : TextView
+    lateinit var map_View: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        map_View = findViewById(R.id.map_view)
+
+        tvDate = findViewById(R.id.tvDate)
+
+        val tvToday = intent.getStringExtra("tvToday")
+
+        tvDate.text = "오늘은 ${tvToday}"
+
+        val mapView = MapView(this)
+        map_View.addView(mapView)
     }
 }
