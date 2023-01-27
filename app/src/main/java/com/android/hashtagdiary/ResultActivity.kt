bab58c5742ad246376550463e42ad3530c031bff
suucong <sophia021210@naver.com>
2023-01-27 16:31:20 +0900

    지도 API 추가

--- app/src/main/java/com/android/hashtagdiary/ResultActivity.kt
+++ app/src/main/java/com/android/hashtagdiary/ResultActivity.kt
@@ -1,20 +1,39 @@
 package com.android.hashtagdiary
 
+import android.Manifest
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.media.audiofx.BassBoost
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresPermission
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_result.*
+import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapView
 import net.daum.mf.map.api.MapPoint
 
 class ResultActivity : AppCompatActivity() {
-    lateinit var tvDate : TextView
+    lateinit var tvDate: TextView
     lateinit var map_View: ConstraintLayout
+    lateinit var mapView: MapView
+    private val ACCESS_FINE_LOCATION = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-        map_View = findViewById(R.id.map_view)
+        map_View = findViewById(R.id.map_View)
 
         tvDate = findViewById(R.id.tvDate)
 
@@ -22,7 +41,47 @@ class ResultActivity : AppCompatActivity() {
 
         tvDate.text = "오늘은 ${tvToday}"
 
-        val mapView = MapView(this)
+        mapView = MapView(this)
         map_View.addView(mapView)
+
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+
+        // 권한이 허용되지 않음
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // 이전에 거부한 적이 있으면 설명(참고)
+                var dlg = AlertDialog.Builder(this)
+                dlg.setTitle("권한이 필요한 이유")
+                dlg.setMessage("사진 정보를 얻기 위해서는 외부 저장소 권한이 필수로 필요합니다.")
+                dlg.setPositiveButton("확인") {dialog, which -> ActivityCompat.requestPermissions(this@ResultActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION)}
+                dlg.setNegativeButton("취소", null)
+                dlg.show()
+            } else {
+                // 처음 권한 요청
+                ActivityCompat.requestPermissions(this@ResultActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION)
+            } else {
+            // 권한이 이미 제대로 허용됨
+            mapView.currentLocationTrackingMode =
+                MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading  //이 부분
+
+            val lm: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val userNowLocation: Location? = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+            //위도 , 경도
+            val uLatitude = userNowLocation?.latitude
+            val uLongitude = userNowLocation?.longitude
+            val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude!!, uLongitude!!)
+
+            // 현 위치에 마커 찍기
+            val marker = MapPOIItem()
+            marker.itemName = "현 위치"
+            marker.mapPoint =uNowPosition
+            marker.markerType = MapPOIItem.MarkerType.BluePin
+            marker.selectedMarkerType = MapPOIItem.MarkerType.RedPin
+            mapView.addPOIItem(marker)
+        }
     }
 }
+
