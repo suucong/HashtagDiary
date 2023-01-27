2aa90e27f7d4e89f44b76bf4159a34c4259bf620
suucong <sophia021210@naver.com>
2023-01-27 12:35:56 +0900

    지도 API 추가중

--- app/src/main/java/com/android/hashtagdiary/ResultActivity.kt
+++ app/src/main/java/com/android/hashtagdiary/ResultActivity.kt
@@ -2,21 +2,10 @@ package com.android.hashtagdiary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
-
-    lateinit var tvDate : TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-
-        tvDate = findViewById(R.id.tvDate)
-
-        val tvToday = intent.getStringExtra("tvToday")
-
-        tvDate.text = "오늘은 ${tvToday},"
-
     }
 }
