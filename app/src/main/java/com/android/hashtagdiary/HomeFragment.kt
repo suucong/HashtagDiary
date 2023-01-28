fa91e2abba2475ca885d0b0aac9e86f4b56dca2b
Keilly <keilly0104@naver.com>
2023-01-28 16:12:36 +0900

    기연_오늘 일기 썼으면 다시 못쓰도록

--- app/src/main/java/com/android/hashtagdiary/HomeFragment.kt
+++ app/src/main/java/com/android/hashtagdiary/HomeFragment.kt
@@ -1,12 +1,18 @@
 package com.android.hashtagdiary
 
 import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,11 +28,15 @@ class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     lateinit var btnRecord : Button
 
+    lateinit var dbManager : DBManager
+    lateinit var sqlitedb : SQLiteDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,10 +45,31 @@ class HomeFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
         btnRecord = view.findViewById(R.id.btnRecord)
+        dbManager = DBManager(requireContext(), "diarybyday", null, 1)
 
+        sqlitedb = dbManager.readableDatabase
+        var cursor : Cursor
+        cursor = sqlitedb.rawQuery("SELECT date FROM diarybyday;", null)
+        var today = LocalDate.now().toString()
+
+        var plag : String = "기록 안함"
         btnRecord.setOnClickListener {
-            var intentRecord = Intent(getActivity(), RecordActivity::class.java)
-            startActivity(intentRecord)
+            while (cursor.moveToNext()) {
+                if (today.equals(cursor.getString(0).toString())) {
+                    Toast.makeText(requireContext(), "이미 오늘의 일기가 기록되었습니다.", Toast.LENGTH_LONG).show()
+                    plag = "기록 완료"
+                    cursor.moveToLast()
+                }
+                else {
+                }
+            }
+
+            if (plag.equals("기록 안함")) {
+                var intentRecord = Intent(getActivity(), RecordActivity::class.java)
+                startActivity(intentRecord)
+            }
+            sqlitedb.close()
+            cursor.close()
         }
         return view
     }
---
+++ app/src/main/java/com/android/hashtagdiary/PrevResultActivity.kt
@@ -0,0 +1,11 @@
+package com.android.hashtagdiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrevResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prev_result)
+    }
+}
